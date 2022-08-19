5ce0d419b81cd5a3f6f681deee0d52292089384b
EternalVigil <theeternalvigil@gmail.com>
2022-08-19 11:36:21 -0700

    solution

--- dontGotTime.js
+++ dontGotTime.js
@@ -28,3 +28,80 @@ we have 4 appointments already on the books
 
 should return [12, 13, 14] in timestamp / human readable form
 */
+
+const staticDay = new Date().setUTCHours(0,0,0,0);
+const currentDayStart = staticDay;
+
+const standardInterval = 0.5;
+const convertedStandardInterval = (standardInterval * 60) * 60000;
+
+const currentDateOpen = new Date(currentDayStart).setUTCHours(12);
+const currentDateClose = new Date(currentDayStart).setUTCHours(16);
+
+const timeSet = new Set();
+
+const createTimeStamp = (hours, minutes) => {
+    const time = new Date(currentDayStart).setUTCHours(hours ?? 0, minutes ?? 0);
+    return time;
+};
+
+const printTime = (input) => {
+    return new Date(input).toUTCString();
+};
+
+const bookedAppointments = [
+    {
+        startTime: createTimeStamp(12, 30),
+        endTime: createTimeStamp(12, 55),
+    },
+    {
+        startTime: createTimeStamp(13, 45),
+        endTime: createTimeStamp(14, 0),
+    },
+    {
+        startTime: createTimeStamp(15, 0),
+        endTime: createTimeStamp(15, 30),
+    },
+    {
+        startTime: createTimeStamp(15, 45),
+        endTime: createTimeStamp(16, 0),
+    },
+];
+
+const intervalCount = (currentDateClose - currentDateOpen) / convertedStandardInterval;
+
+let timeIndex = currentDateOpen;
+
+for(let i = 0; i < intervalCount; i++) {
+    const desiredEnd = timeIndex + convertedStandardInterval;
+
+    let conflict = false;
+
+    bookedAppointments.forEach((booked) => {
+        const { startTime, endTime } = booked;
+
+        const overlappingHead = startTime < desiredEnd && endTime >= desiredEnd;
+
+        const overlappingTail = startTime <= timeIndex && endTime > timeIndex;
+
+        const insideInterval = startTime >= timeIndex && endTime <= desiredEnd;
+
+        const exactMatch = startTime === timeIndex && endTime === desiredEnd;
+        
+        if(overlappingHead || overlappingTail || insideInterval || exactMatch) {
+            conflict = true;
+        }
+
+    });
+
+    if(conflict === false) {
+        timeSet.add(timeIndex);
+    }
+
+    timeIndex = desiredEnd;
+}
+
+const times = Array.from(timeSet);
+const convertTimes = times.map((time) => new Date(time).toUTCString());
+console.log('times', times);
+console.log('converted times', convertTimes);
