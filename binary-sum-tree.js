22bde54c43d7633c31361ab0d4b53d147a91b250
EternalVigil <theeternalvigil@gmail.com>
2022-08-21 15:21:15 -0700

    new problem

---
+++ binary-sum-tree.js
@@ -0,0 +1,17 @@
+/*
+Create a binary sum tree and a function / series of functions
+to validate that is a real sum tree
+
+valid example
+     29
+  10   03
+04 06    03
+       02  01
+
+
+invalid example
+    29
+  10   03
+04 06    03
+       02  02
+*/
