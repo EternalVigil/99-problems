edcf3f9cb508ee5c193bffbc148551b1311be2dd
EternalVigil <theeternalvigil@gmail.com>
2022-08-21 15:42:23 -0700

    new problem

---
+++ matrix-travels.js
@@ -0,0 +1,32 @@
+/*
+
+Given a point in a matrix
+see if you can reach a given destination point
+is less or equal to a given number of moves
+
+S = start
+F = finish
+
+Map 1
+3 moves allowed
+[
+    [[S], [ ], [ ]]
+    [[ ], [ ], [ ]]
+    [[ ], [ ], [ ]]
+    [[ ], [ ], [F]]
+    [[ ], [ ], [ ]]
+    [[ ], [ ], [ ]]
+]
+
+Map 2
+5 moves
+[
+    [[S], [ ], [ ]]
+    [[ ], [ ], [ ]]
+    [[ ], [ ], [ ]]
+    [[ ], [ ], [ ]]
+    [[ ], [ ], [ ]]
+    [[ ], [F], [ ]]
+]
+
+*/
