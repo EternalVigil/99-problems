30c31a921bac7e4a1b0baccbae73457a67175a9f
EternalVigil <theeternalvigil@gmail.com>
2022-08-21 17:02:56 -0700

    new problem

---
+++ string-match.js
@@ -0,0 +1,22 @@
+/*
+
+given a string paragraph and an input string
+find all the instances of the input string within the given paragraph
+and output the paragraph with all the matches bracketed
+matches must be case-sensitive
+
+example 
+given
+This is the best version of the thing I love 
+and nothing could ever hope to replace it
+I hope that this thing will stay with me forever
+despite the slow match of time
+
+input 
+the
+*/
+
+const given = 'This is the best version of the thing I love and nothing could ever hope to replace it.  I hope that this thing will stay with me forever, despite the slow match of time';
+
+const input = 'the';
+
