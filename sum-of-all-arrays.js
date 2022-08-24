7c0e2395ded01739473b345915665d46e99dcbb7
EternalVigil <theeternalvigil@gmail.com>
2022-08-24 09:39:00 -0700

    expanded solution

--- sum-of-all-arrays.js
+++ sum-of-all-arrays.js
@@ -9,8 +9,10 @@ outputs
 [6,8,10,12]
 */
 
-const arrayA = [1,2,3,4,20];
-const arrayB = [5,6,7,8,10,15];
+const arrayA = [ 1, 2, 3, 4,20];
+const arrayB = [ 5, 6, 7, 8,10,15];
+const arrayC = [10,12,14,16,18,20,40];
+const arrayD = [ 1, 2, 4, 6, 8];
 
 const sumOfArrays = (A, B) => {
     const longestLength = Math.max(A.length, B.length);
@@ -24,5 +26,35 @@ const sumOfArrays = (A, B) => {
     return sumArray;
 };
 
-const output = sumOfArrays(arrayA, arrayB);
-console.log('output', output);
+// const output = sumOfArrays(arrayA, arrayB);
+// console.log('output', output);
+
+const sumOfAllArrays = (arrays) => {
+    const sumArray = [];
+    const arraysLength = arrays.length;
+
+    let longestArrayLength = 0;
+
+    arrays.forEach((array) => {
+        if(array.length > longestArrayLength) {
+            longestArrayLength = array.length;
+        }
+    });
+
+    // console.log('longest', longestArrayLength);
+
+    for(let i = 0; i < arrays.length; i++) {
+        // console.log(arrays[i]);
+
+        for(let j = 0; j < arrays[i].length; j++) {
+            // console.log(arrays[i][j]);
+
+            sumArray[j] = (sumArray[j] ?? 0) + arrays[i][j];
+        }
+    }
+
+    return sumArray;
+}
+
+const sums = sumOfAllArrays([arrayA, arrayB, arrayC, arrayD]);
+console.log('sums', sums);
